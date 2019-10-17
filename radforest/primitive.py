5fc83d10d56da5feb0ad263b5b6321d30740ead9
Raja <argram30@gmail.com>
2019-10-17 22:08:13 +0200

    created

---
+++ radforest/primitive.py
@@ -0,0 +1,19 @@
+"""
+primitive module.
+
+This module is the core of the radforest library.
+"""
+
+
+class Primitive(object):
+    """
+    Primitive class.
+
+    *to radiance
+    *from radiance
+    *to  JSON
+    *from JSON
+
+    """
+    def __init__(self):
+        pass
---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = 'radforest',
+    version = '0.1.0',
+    description = 'This is the rad forest generator',
+    url = '',
+    Author = 'Raja',
+    author_email = 'raja@raja.raja',
+    license = 'MIT',
+    packages = ['radforest']
+)
